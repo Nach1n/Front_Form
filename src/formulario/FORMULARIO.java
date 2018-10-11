6fbae0049bd5e4289a3cc1ee7d5641ef274b6168
liapino <l.ancavilpino@gmail.com>
2018-10-11 19:51:06 -0300

    Front v.1.0

---
+++ src/formulario/FORMULARIO.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package formulario;
+
+
+/**
+ *
+ * @author luisignacio
+ */
+public class FORMULARIO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FORMULARIO_1 FORM = new FORMULARIO_1();
+        FORM.setVisible(true);
+        
+    }
+    
+}
